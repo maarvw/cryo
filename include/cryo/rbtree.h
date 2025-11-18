3fc5ca530fb66ecc20c85c421c84be5e3de56f23
marvindavid.wagner <marvindavid.wagner@stud.uni-goettingen.de>
2025-11-18 16:45:34 +0100

    expanded & cleaned up

---
+++ include/cryo/rbtree.h
@@ -0,0 +1,193 @@
+#include <algorithm>
+#include <initializer_list>
+#include <stdexcept>
+#include <iostream>
+//#include "../../external/fe/include/fe/arena.h"
+namespace cryo {
+
+/*radix balanced tree with persistent functionality, 
+  serves as underlying datastructure for containers*/
+template<typename T>
+class rbtree {
+
+private:
+    static const int B = 5;
+    static const int M = 1 << B;
+
+    /*node of the rbtree*/
+    struct node {
+        node* inner[M];
+        T leaf[M];
+    };
+
+    /*root node of the tree, unique to every persistent "copy"*/
+    node* root;
+    /*numer of elements in the tree*/
+    size_t size;
+    /*bitshift amount at the root level. power of 2.*/
+    int shift;
+    /*current capacity of the tree. multiple of M. if full, add() creates new layer*/
+    int capacity;
+
+    /*initialises empty tree to be used by constructors*/
+    void empty_init() {
+        size = 0;
+        root = new node;
+        shift = 0; //bei erhöhung der höhe +B
+        capacity = M;
+    }
+
+    /*contructor for new tree, only intended to be used by update functions*/
+    rbtree(node* r, size_t s, int sf, int cap) {
+        root=r, size=s, shift=sf, capacity=cap;
+    }
+
+    /*adds element in non-persistent way, not intended to be used outside of constructors*/
+    void add_primitive(T elem) {
+        size++;
+        if (size>=capacity) { //new root
+            int s = shift;
+            node* newroot = new node; 
+            newroot->inner[0]=root;
+            newroot->inner[1]= new node;
+            node* newnode = newroot->inner[1]; //newroot remains "root" of new subtree
+            while (s!=0) {
+                newnode->inner[0] = new node;
+                newnode=newnode->inner[0];
+                s -= B;
+            }
+            newnode->leaf[0]=elem;
+            root = newroot;
+            size++;
+            shift+=B;
+            capacity*=M;
+            return;
+        }
+        node* cur = root;
+        int i = size-1;
+        int s = shift;
+        while (s != 0) {
+            if (cur->inner[(i>>s)%M]==NULL) {
+                cur->inner[(i>>s)%M] = new node;
+            }
+            cur = cur->inner[(i >> s) % M];
+            s -= B;
+        }
+        cur->leaf[i%M]=elem;
+    }
+
+    
+    /*recursive helper function adding an element at a specific index in the tree,
+      expanding the tree if necessary. creates new nodes and returns a new root element
+      for the new persistent "copy"*/
+    node* insert_at_index(T elem, node* cur, int s, size_t i) {
+        if (s==0) {
+            //copy & change leaf
+            node* newleaf = new node;
+            std::copy_n(cur->leaf, M, newleaf->leaf);
+            newleaf->leaf[i%M]=elem;
+            return newleaf;
+        }
+        node* newinner = new node;
+        std::copy_n(cur->inner, M, newinner->inner);
+        if (cur->inner[(i>>s)%M]==NULL) { //need to build new subtree of inners
+            node* newnode = newinner; //newinner remains "root" of new subtree
+            while (s!=0) {
+                newnode->inner[(i>>s)%M] = new node;
+                newnode=newnode->inner[(i>>s)%M]; //(i>>s)%M should always be 0 in this loop but just in case
+                s -= B;
+            }
+            newnode->leaf[0]=elem;
+            return newinner;
+        }
+        newinner->inner[(i>>s)%M]=insert_at_index(elem, cur->inner[(i>>s)%M], s-B, i);
+        return newinner;
+    }
+
+    
+public:
+
+    /*default constructor for empty tree*/
+    rbtree() {
+        empty_init();
+    }
+
+    /*single element constructor*/
+    rbtree(T elem) {
+        empty_init();
+        add_primitive(elem);
+    }
+
+    /*constructor for initializer list*/
+    rbtree(std::initializer_list<T> elems) {
+        empty_init();
+        for (T elem : elems)
+            add_primitive(elem);
+    }
+
+    /*adds new element to the tree, expanding the tree if necessary.
+      returns a persistent "copy" of the previous tree*/
+    rbtree add(T elem) {
+        if (size+1 >= capacity) { //tree full, need to expand depth
+            int s = shift;
+            node* newroot = new node; 
+            newroot->inner[0]=root;
+            newroot->inner[1]= new node;
+            node* newnode = newroot->inner[1]; //newroot remains "root" of new subtree
+            while (s!=0) {
+                newnode->inner[0] = new node;
+                newnode=newnode->inner[0];
+                s -= B;
+            }
+            newnode->leaf[0]=elem;
+            return rbtree(newroot, size+1, shift+B, capacity*M);
+        }
+        node* newroot = insert_at_index(elem, root, shift, size);
+        return rbtree(newroot, size+1, shift, capacity);
+    }
+
+    /*adds whole list of elements at once but no fancy transience stuff (yet)*/
+    rbtree add(std::initializer_list<T> elems) {
+        rbtree newtree;
+        for (T elem : elems) {
+            newtree = newtree.add(elem);
+        }
+        return newtree;
+    }
+
+    /*inserts an element at a specific (previously existing) index in the tree, 
+      overwriting the data at that index. returns a persistent "copy" of the previous tree.*/
+    rbtree insert(size_t i, T elem) {
+        if (i>=size)
+            throw std::runtime_error("dummer hurensohn");
+        node* newroot = insert_at_index(elem, root, shift, i);
+        return rbtree(newroot, size, shift, capacity);
+    }
+
+    /*returns element at specific index, making no changes.
+      equivalent to the [] operator*/
+    const T& get(size_t i) {
+        if (i>=size) 
+            throw std::runtime_error("dummer hurensohn"); //vielleicht nochmal ändern lol
+        node* cur = root;
+        int s = shift;
+        while (s != 0) {
+            cur = cur->inner[(i >> s) % M];
+            s -= B;
+        }
+        return cur->leaf[i%M];
+    }
+
+    /*returns element at specific index, making no changes.
+      equivalent to the get() function*/
+    const T& operator[] (size_t i) {
+        return get(i);
+    }
+
+    /*returns number of elements in the tree*/
+    int getSize() {
+        return size;
+    }
+};
+
+}
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,9 +1,39 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <doctest/doctest.h>
-#include <cryo/set.h>
+#include<doctest/doctest.h>
+#include<cryo/set.h>
+#include"cryo/rbtree.h"
+#include<iostream>
+using namespace std;
 
 TEST_CASE("Arena") {
     cryo::Set set(23);
     CHECK(set.key() == 23);
+}
+
+TEST_CASE("adding-stuff") {
+    cryo::rbtree knecht1 = cryo::rbtree<int>();
+    cryo::rbtree knecht2 = knecht1.add(1);
+    CHECK(knecht1.getSize() == 0);
+    CHECK(knecht2.getSize() == 1);
+    CHECK(knecht2[0] == 1);
+
+    cryo::rbtree<int> knechte[50];
+    for (int i=1; i<50; i++){
+        knechte[i]=knechte[i-1].add(i);
+    }
+
+    cryo::rbtree knecht3 = knechte[45];
+
+    CHECK(knecht3[25] == 26);
+    cryo::rbtree knecht4 = knecht3.insert(25, 1);
+    CHECK(knecht3[25] == 26);
+    CHECK(knecht4[25] == 1);
+}
+
+TEST_CASE("constructor-list") {
+    cryo::rbtree<int> k1({1, 2,3,4,5,6,7,8});
+    cout<<k1[7]<<endl;
+    CHECK(k1[7] == 8);
+    CHECK(k1.getSize() == 8);
 }
